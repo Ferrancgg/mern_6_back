808f09b5f14ffb82bb8e608221e17b091ab3a09c
TuNombreDeUsuario <ferrancgg@gmail.com>
2024-12-30 20:00:25 +0100

    conexion a la BBDD lista. siguiente paso crear modelos

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 console.log("hola mundo")
 const express=require("express")
+const { connectBD } = require("./src/confing/db")
+require ("dotenv").config()
 const app=express()
+
 const PORT=4001
+connectBD()
 app.use("/api/v1",(req,res,next)=>{
     return res.status(200).json("esto funciona")
 })
---
+++ src/confing/db.js
@@ -0,0 +1,8 @@
+const mongoose=require("mongoose")
+const connectBD=async()=>{
+    try{await mongoose.connect(process.env.BD_URL)
+        console.log("conectado con exito a la BBDD")
+    }
+    catch (error){console.log("error al conectar la BBDD")}
+}
+module.exports={connectBD}
